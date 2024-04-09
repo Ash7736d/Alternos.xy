ca97fe005b63e13efae532383dca535af61c4173
Ash <150865599+Ash7736d@users.noreply.github.com>
2024-04-09 12:21:21 +0800

    Update and rename shopscript.js to script.js

---
+++ pages/script.js
@@ -0,0 +1,5 @@
+// Add event listener to the button to navigate to the main directory
+document.getElementById('homeButton').addEventListener('click', function(event) {
+  event.preventDefault(); // Prevent the default action
+  window.location.href = '../index.html'; // Navigate to the index.html in the main directory
+});
--- pages/shopscript.js
+++
@@ -1,5 +0,0 @@
-// Add event listener to the button to navigate to the shop page
-document.getElementById('homeButton').addEventListener('click', function(event) {
-  event.preventDefault(); // Prevent the default action
-  window.location.href = 'index.html';
-});

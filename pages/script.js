a0565bdd07ba93b98cc09e0fe1be773594511f8e
Ash <150865599+Ash7736d@users.noreply.github.com>
2024-04-10 01:47:44 +0800

    Update script.js

--- pages/script.js
+++ pages/script.js
@@ -1,5 +1,5 @@
 // Add event listener to the button to navigate to the main directory
-document.getElementById('homeButton').addEventListener('click', function(event) {
+document.getElementById('shopButton').addEventListener('click', function(event) {
   event.preventDefault(); // Prevent the default action
   window.location.href = '../index.html'; // Navigate to the index.html in the main directory
 });

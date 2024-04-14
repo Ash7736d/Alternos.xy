ee99b6a89485a605466efc04da4d82761a541cc7
Ash <150865599+Ash7736d@users.noreply.github.com>
2024-04-14 21:05:24 +0800

    Update script.js

--- pages/script.js
+++ pages/script.js
@@ -28,30 +28,30 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     // Set the date we're counting down to
-var countDownDate = new Date("Apr 30, 2024 12:00:00").getTime();
+    var countDownDate = new Date("May 15, 2024 00:00:00").getTime();
 
-// Update the countdown every 1 second
-var x = setInterval(function() {
-
-  // Get the current date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now and the countdown date
-  var distance = countDownDate - now;
-
-  // Calculate days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the countdown in the element with id="countdown"
-  document.getElementById("countdown").innerHTML = days + "𝗱 " + hours + "𝗵 " + minutes + "𝗺 " + seconds + "𝘀 ";
-
-  // If the countdown is over, display a message
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("countdown").innerHTML = "𝗢𝘂𝘁 𝗼𝗳 𝗦𝘁𝗼𝗰𝗸";
-    }
-  }, 1000);
+    // Update the countdown every 1 second
+    var x = setInterval(function() {
+        // Get the current date and time
+        var now = new Date().getTime();
+        
+        // Calculate the distance between now and the countdown date
+        var distance = countDownDate - now;
+        
+        // Calculate days, hours, minutes, and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+        // Display the countdown
+        var countdownElement = document.querySelector(".booter");
+        countdownElement.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+        
+        // If the countdown is over, display a message
+        if (distance < 0) {
+            clearInterval(x);
+            countdownElement.innerHTML = "EXPIRED";
+        }
+    }, 1000);
 });
